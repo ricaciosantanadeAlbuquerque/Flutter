93738e5dc18c9fb4ad13e69959d91d8bfbeb1ac9
ricacio <ricaciozz@gmail.com>
2022-12-17 18:19:48 -0300

    17-12-2022

--- Telas_navegadas/lib/segunda_tela.dart
+++ Telas_navegadas/lib/segunda_tela.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:login/gerente.dart';
 
 import 'controlle.dart';
 
@@ -17,64 +18,59 @@ class homePageEstado extends State<StatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:const Center(
-            child: Text("Home Page")
-        )
-      ),
-      body:Container(
-          child: Center(
+        drawer: Drawer(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                  GestureDetector(
-                    child: Text("Contador $contador"),
-                    onTap:(){
-                      setState((){
-                        contador++;
-                      });
-                    }
-                    ),
-                    Container(
-                      child: Switch(
-                      value: Controle.instancia.troca,
-                      onChanged:(valeu){
-                        setState((){
-                          Controle.instancia.trocarValor();
-                        });
-                      }
-                      )
-                    ),
-                    Row(
-                      mainAxisAlignment:MainAxisAlignment.spaceAround,
-                      children:<Widget> [
-                        Container(
-                          width: 50,
-                          height:50,
-                          color: Colors.black
-                        ),
-                        Container(
-                           width: 50, height: 50, color: Colors.black
-                        ),
-                        Container(
-                           width: 50, height: 50, color: Colors.black
-                        )
-                      ]
-                    )
-              ]
+          children: <Widget>[
+            UserAccountsDrawerHeader(currentAccountPicture: ClipRRect(borderRadius: BorderRadius.circular(40), child: Image.asset("assets/11.jpg")), accountName: const Text("RIcacio"), accountEmail: const Text("ricaciozz@gmial.com")),
+            const ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Incio"),
+              subtitle: Text("homePage"),
             ),
-          )
-      ),
-      floatingActionButton:FloatingActionButton(
-        onPressed: (){
-          setState((){
-            contador++;
-          });
-        },
-        child: const Icon(Icons.add)
-        )
-    );
+            ListTile(
+                leading: const Icon(Icons.restart_alt),
+                trailing: const Icon(Icons.delete),
+                title: const Text(" Sair"),
+                subtitle: const Text("voltar"),
+                onTap: () {
+                  setState(() {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                      return const Gerente();
+                    }));
+                  });
+                })
+          ],
+        )),
+        appBar: AppBar(title: const Center(child: Text("Home Page"))),
+        body: Container(
+            child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            GestureDetector(
+                child: Text("Contador $contador"),
+                onTap: () {
+                  setState(() {
+                    contador++;
+                  });
+                }),
+            Container(
+                child: Switch(
+                    value: Controle.instancia.troca,
+                    onChanged: (valeu) {
+                      setState(() {
+                        Controle.instancia.trocarValor();
+                      });
+                    })),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[Container(width: 50, height: 50, color: Colors.black), Container(width: 50, height: 50, color: Colors.black), Container(width: 50, height: 50, color: Colors.black)])
+          ]),
+        )),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                contador++;
+              });
+            },
+            child: const Icon(Icons.add)));
   }
 }
-
-
