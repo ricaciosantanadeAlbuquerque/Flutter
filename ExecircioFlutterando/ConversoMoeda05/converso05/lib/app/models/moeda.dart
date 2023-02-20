4af8298781ae909baa2fc9d723be2955ba3ef06c
ricacio <ricaciozz@gmail.com>
2023-02-20 17:33:15 -0300

    20-02-2023

---
+++ ExecircioFlutterando/ConversoMoeda05/converso05/lib/app/models/moeda.dart
@@ -0,0 +1,14 @@
+class Moeda {
+  // atribultos de classe
+  final String nome;
+  final double real;
+  final double dolar;
+  final double euro;
+  final double bitcoin;
+
+  Moeda(this.nome, this.real, this.dolar, this.euro, this.bitcoin); // construtor
+
+  static List<Moeda> getMoeda(){ // funcao de classe
+    return null
+  }
+}
