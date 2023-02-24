d7ebb2127bdbf1b2d2a30453259a864f61818756
ricacio <ricaciozz@gmail.com>
2023-02-24 17:35:56 -0300

    24-02-2023

---
+++ Flutter_udemy/projeto03/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+ final List<String> perguntas = ["Qual é a sua cor favorita ?"];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(
+          title:const Center(
+            child: Text("Pergunta")
+          ),
+        ),
+        body:Column(
+          children: <Widget>[
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(onPressed:(){
+
+            },child: const Text("Resposta1"),
+            )
+          ]
+        )
+      )
+    );
+  }
+}
