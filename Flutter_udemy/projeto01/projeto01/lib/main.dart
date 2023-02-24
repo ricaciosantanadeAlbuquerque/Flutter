996ca2ab527794f9d3092f81630d63591127abd6
ricacio <ricaciozz@gmail.com>
2023-02-24 16:41:01 -0300

    24-02-2023

--- Flutter_udemy/projeto01/projeto01/lib/main.dart
+++ Flutter_udemy/projeto01/projeto01/lib/main.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 void main(){
-  
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget{
+  const PerguntaApp({super.key});
+  // toda classe  que é um StatelessWidget deve implementar o método build()
+
+  @override
+  Widget build(BuildContext context){
+    return  const MaterialApp(
+        home: Text("Olá Flutter")
+    );
+  }
+  // está sendo retornado uma instânia  da classe MaterialApp(), que por sua vez possui um parâmetro nomeado que possui uma instância da classe Text()
 }
