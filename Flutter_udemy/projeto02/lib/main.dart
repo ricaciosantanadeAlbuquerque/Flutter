157e5ad20908ce2eccf21427f75b1e0cca6ac0cf
ricacio <ricaciozz@gmail.com>
2023-02-24 17:03:28 -0300

    24-02-2023

---
+++ Flutter_udemy/projeto02/lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:  const Center(
+            child: Text("Perguntas")
+            )
+        ),
+        body:const Text("Olá Flutter!!!",
+        style:TextStyle(
+          fontSize:20.0
+        )
+        )
+      )
+    );
+  }
+}
