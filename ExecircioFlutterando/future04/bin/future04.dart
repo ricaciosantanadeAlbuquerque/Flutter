d1881ba80323a82104b0f9b19d8fa42425ad65f3
ricacio <ricaciozz@gmail.com>
2023-02-26 02:33:08 -0300

    25-02-2023

---
+++ ExecircioFlutterando/future04/bin/future04.dart
@@ -0,0 +1,10 @@
+import 'package:future04/future04.dart' as future04;
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments){
+
+}
+void api()async{
+  var URL = 'https://jsonplaceholder.typicode.com/todos/1';
+     var response = await http.get();
+}
---
+++ ExecircioFlutterando/future04/test/future04_test.dart
@@ -0,0 +1,8 @@
+import 'package:future04/future04.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
