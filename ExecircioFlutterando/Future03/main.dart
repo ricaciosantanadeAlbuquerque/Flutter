998d1aacd11e291aa5d59380ddbf1e86472d5b8b
ricacio <ricaciozz@gmail.com>
2023-02-26 01:42:19 -0300

    25-02-2023

---
+++ ExecircioFlutterando/Future03/main.dart
@@ -0,0 +1,12 @@
+// Transformando função em  async
+void main() {
+  print4();
+  print(" 1 ");
+  print(" 2 ");
+  print(" 3 ");
+}
+
+Future<void> print4() async {
+   await Future.delayed(Duration(seconds: 2));
+  print(4);
+}
