6b634d3a71169df625b878d3d72c8c3793872173
ricacio <ricaciozz@gmail.com>
2023-02-28 01:40:39 -0300

    28-02-2023

---
+++ ExecircioFlutterando/future06/bin/future06.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  var future = Future.delayed(Duration(seconds: 2));
+  future.then((value) => print('4'));
+  print(" 1 ");
+  print(" 2 ");
+  print(" 3 ");
+  
+}
