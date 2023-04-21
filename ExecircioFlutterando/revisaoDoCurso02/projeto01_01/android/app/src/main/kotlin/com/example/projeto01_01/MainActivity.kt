5d0ed6e9d3fe03c0aa4839d82d3d20ec5dc4f29c
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-20 21:26:34 -0300

    20-04-2023

---
+++ ExecircioFlutterando/revisaoDoCurso02/projeto01_01/android/app/src/main/kotlin/com/example/projeto01_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projeto01_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ExecircioFlutterando/revisaoDoCurso02/projeto01_01/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+ const PerguntaApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+         appBar:AppBar(
+           title: const Center(child: Text("Perguntas"))
+         ),
+         body: const Text("Olá Flutter !")
+      )
+    );
+  }
+}
