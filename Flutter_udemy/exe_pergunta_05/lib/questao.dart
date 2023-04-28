5c30e9a810cd0f2fc96d1641fbd708d5fc2fb2ec
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-27 21:45:59 -0300

    margin add

--- Flutter_udemy/exe_pergunta_05/lib/questao.dart
+++ Flutter_udemy/exe_pergunta_05/lib/questao.dart
@@ -9,6 +9,7 @@ class Questao extends StatelessWidget { // classe Imutável
   Widget build(BuildContext context) {
     return Container(
       width:double.infinity,
+      margin:const EdgeInsets.all(10),
       child:Text(
         texto,
         style:const TextStyle(
