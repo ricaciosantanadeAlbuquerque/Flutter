073303864a9f9566546511febcafcacaa30ba07f
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-28 02:15:50 -0300

    28-04-2023

--- Flutter_udemy/exe_pergunta_07/lib/main.dart
+++ Flutter_udemy/exe_pergunta_07/lib/main.dart
@@ -45,9 +45,14 @@ class PerguntaAppState extends State<PerguntaApp> {
 
     List<Widget> respostas = [];
 
-    for (var valor in perguntas[index]['resposta'] as List<String>) {
+    for (var valor in perguntas[index]['resposta'] as List<String>) { //  
       respostas.add(Resposta(texto: valor, funcao: responder));
     }
+    /**
+     * perguntas[index]['resposta'] o retorn disto é  ['Preto', 'Vermelho', 'Verde', 'Branco']
+     * quando acessamos pergunta na posição[index] ou 0, estamos acessando um Map{} que possui chaves e valores, para acessar os valores em um Map basta referênciar a chave desejada ['resposta'], neste caso retornou uma lista de String. 
+     * E é  essa lista de Strings que está cendo percorrida pelo for( in )
+     */
 
     return MaterialApp(
          home:Scaffold(
@@ -61,7 +66,8 @@ class PerguntaAppState extends State<PerguntaApp> {
               Questao(texto: perguntas[index]['texto'] as String ),
               ...respostas
               /**
-               * O
+               * O operador Spred '...' pega cada elemento da lista 'respostas' e joga para dentro da lista Column([]), um por um 
+               * de forma ordenada.
                */
             ]
           )
