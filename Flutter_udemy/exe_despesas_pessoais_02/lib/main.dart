d9872674208f35bd6a12b7a58545c19202cd4018
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-06-12 01:21:46 -0300

    exe despesas Pessoais 02 inicio

---
+++ Flutter_udemy/exe_despesas_pessoais_02/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  const MyHomePage({super.key});
+   @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+          title:const Text("Despesas Pessoais")
+      ),
+      body:Column(
+          children: <Widget>[
+              Container(
+                width:double.infinity,
+                child: const Card(
+                  color: Colors.blue,
+                  elevation:5,
+                  child: Text('Gráfico'),
+                ),
+              ),
+              const Card(
+                child: Text('Lista de Transações')
+              )
+          ]
+      )
+    );
+  }
+}
