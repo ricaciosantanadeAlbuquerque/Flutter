e45215601b415cd94daae3ba67cdf429f473ec88
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-06-12 00:50:31 -0300

    inicializando_exe_despesas

---
+++ Flutter_udemy/exe_despesas_pessoais_01/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp( const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+    );
+  }
+}
