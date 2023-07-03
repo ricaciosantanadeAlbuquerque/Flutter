47f507a10b0c9a25f64ba34a3de3fae98b890f9b
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-03 02:16:18 -0300

    componentizando

--- Flutter_udemy/exe_despesas_pessoais_10/lib/components/transaction_forme.dart
+++ Flutter_udemy/exe_despesas_pessoais_10/lib/components/transaction_forme.dart
@@ -15,34 +15,37 @@ class TransactionForme extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          children: [
-            TextField(
-                controller: title,
-                onSubmitted: (value) => submit(),
-                decoration: const InputDecoration(
-                  labelText: 'Título',
-                )),
-            TextField(
-             controller: value,
-             onSubmitted: (value) => submit(),
-             keyboardType:const TextInputType.numberWithOptions(),
-             decoration: const InputDecoration(labelText: 'Valor (R\$)'),
-             ),
-            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-              TextButton(
-                onPressed: submit,
-                child: const Text(
-                  'Nova Transação',
-                  style: TextStyle(color: Colors.purple),
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [
+              TextField(
+                  controller: title,
+                  onSubmitted: (value) => submit(),
+                  decoration: const InputDecoration(
+                    labelText: 'Título',
+                  )),
+              TextField(
+               controller: value,
+               onSubmitted: (value) => submit(),
+               keyboardType:const TextInputType.numberWithOptions(),
+               decoration: const InputDecoration(labelText: 'Valor (R\$)'),
+               ),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                TextButton(
+                  onPressed: submit,
+                  child: const Text(
+                    'Nova Transação',
+                    style: TextStyle(color: Colors.purple),
+                  ),
                 ),
-              ),
-            ])
-          ],
+              ])
+            ],
+          ),
         ),
       ),
     );
