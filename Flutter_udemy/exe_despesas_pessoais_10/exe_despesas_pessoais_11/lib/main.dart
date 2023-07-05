c3f73a682d17e4984dcab2976a9037d9d7fa889c
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-05 03:06:42 -0300

    add Modal com formulário

--- Flutter_udemy/exe_despesas_pessoais_10/exe_despesas_pessoais_11/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_10/exe_despesas_pessoais_11/lib/main.dart
@@ -35,20 +35,40 @@ class _MyHomePageState extends State<MyHomePage> {
       _listTransaction.add(newTransaction);
     });
   }
-
+ _openTransactionFormModal(BuildContext context){
+  showModalBottomSheet(
+    context:context,
+    builder:(_){
+      return   TransactionForm(onSubmit: _addTransaction);
+    }
+  );
+ }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Despesas Pessoais')),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Card(color: Colors.blue, elevation: 5, child: Text('Gráfico')),
-          TransactionForm(onSubmit: _addTransaction),
-          TransactionList(lista: _listTransaction),
-
-        ],
+      appBar: AppBar(title: const Text('Despesas Pessoais'),
+      actions:[
+        IconButton(onPressed:() => _openTransactionFormModal(context),
+        icon:const Icon(Icons.add),
+        ),
+      ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Card(color: Colors.blue, elevation: 5, child: Text('Gráfico'),
+            ),
+            TransactionList(lista: _listTransaction),
+      
+          ],
+        ),
+      ),
+      floatingActionButton:FloatingActionButton(
+        onPressed:() => _openTransactionFormModal(context),
+        child:const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation:FloatingActionButtonLocation.centerFloat,
     );
   }
 }
