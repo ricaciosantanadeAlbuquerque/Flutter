eb58c6e90201c246274af30ce5e99b15418c0a4b
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-10 03:48:07 -0300

    componentizando

---
+++ Flutter_udemy/exe_despesas_pessoais_14/lib/components/transaction_form.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatefulWidget {
+  final void Function(String, double) onSubmit;
+  const TransactionForm({super.key, required this.onSubmit});
+  @override
+  State<TransactionForm> createState() {
+    return TransactionFormState();
+  }
+}
+
+class TransactionFormState extends State<TransactionForm> {
+  final title = TextEditingController();
+  final valeu = TextEditingController();
+
+  _Submit() {
+    final titulo = title.text;
+    final valor = double.tryParse(valeu.text) ?? 0.0;
+    if (titulo.isEmpty || valor <= 0) {
+      return;
+    }
+
+    widget.onSubmit(titulo, valor);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(children: [
+          TextField(
+            controller: title,
+            onSubmitted:(_) => _Submit(),
+            decoration: const InputDecoration(
+              labelText: 'Título',
+            ),
+          ),
+          TextField(
+            controller: valeu,
+            keyboardType:const TextInputType.numberWithOptions(decimal:true),
+            onSubmitted:(_) => _Submit(),
+            decoration: const InputDecoration(labelText: 'Valor (R\$)'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: _Submit,
+                child: const Text(
+                  'Nova transação',
+                  style: TextStyle(
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- Flutter_udemy/exe_despesas_pessoais_14/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_14/lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
+import 'package:exe_despesas_pessoais_14/components/transaction_form.dart';
 import 'package:exe_despesas_pessoais_14/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
+import 'components/transaction_list.dart';
+
 
 void main() => runApp(const ExpensesApp());
 
@@ -21,70 +26,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
+ 
   final List<Transaction> _litTransaction = [
     Transaction(id: 't1', title: 'Novo Tênis de corrida', value: 310.76, date: DateTime.now(),
     ),
     Transaction(id: 't2', title: 'Conta de luz ', value: 211.30, date: DateTime.now(),
     ),
   ];
+
+  addTransaction(String titlo,double valor){
+   final newTransaction = Transaction(id: Random().nextDouble().toString(), title: titlo, value: valor, date: DateTime.now());
+
+   setState(() {
+     _litTransaction.add(newTransaction);
+   });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(
         title:const Text('Despesas Pessoais'),
       ),
-      body:Column(
-        crossAxisAlignment:CrossAxisAlignment.stretch,
-        children:[
-          const Card(
-            elevation:5,
-            color:Colors.blue,
-            child: Text('Gráfico')
-          ),
-          Column(
-            children: _litTransaction.map((trs){
-                return Card(
-                  child:Row(
-                    children:[
-                      Container(
-                        margin:const EdgeInsets.symmetric(
-                          horizontal:15,
-                          vertical:10,
-                        ),
-                        padding:const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width:2,
-                            color:Colors.purple
-                          ),
-                        ),
-                        child:Text('R\$ ${trs.value.toStringAsFixed(2)}'
-                        ,style:const TextStyle(
-                            color:Colors.purple,
-                            fontSize: 20,
-                            fontWeight:FontWeight.bold,
-                        ),),
-                      ),
-                      Column(
-                        crossAxisAlignment:CrossAxisAlignment.start,
-                        children:[
-                          Text(trs.title,style:const TextStyle(
-                            fontSize: 16,
-                            fontWeight:FontWeight.bold,
-                          ),
-                          ),
-                          Text(DateFormat('d MMM y').format(trs.date),style: const TextStyle(
-                            color:Colors.grey
-                          ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-            }).toList(),
-          ),
-        ],
+      body:SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment:CrossAxisAlignment.stretch,
+          children:[
+            const Card(
+              elevation:5,
+              color:Colors.blue,
+              child: Text('Gráfico')
+            ),
+          TransactionForm(onSubmit: addTransaction,),
+          TransactionList(lista: _litTransaction)
+          
+          ],
+          
+        ),
       ),
     );
   }
