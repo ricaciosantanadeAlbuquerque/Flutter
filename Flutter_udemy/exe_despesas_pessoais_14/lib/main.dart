0855cc1a46d8ab7dc6877a4bbcc27afae210474d
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-10 04:02:17 -0300

    scroll

--- Flutter_udemy/exe_despesas_pessoais_14/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_14/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 
 import 'components/transaction_list.dart';
 
-
 void main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
@@ -26,44 +25,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
- 
   final List<Transaction> _litTransaction = [
-    Transaction(id: 't1', title: 'Novo Tênis de corrida', value: 310.76, date: DateTime.now(),
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
     ),
-    Transaction(id: 't2', title: 'Conta de luz ', value: 211.30, date: DateTime.now(),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz ',
+      value: 211.30,
+      date: DateTime.now(),
     ),
   ];
 
-  addTransaction(String titlo,double valor){
-   final newTransaction = Transaction(id: Random().nextDouble().toString(), title: titlo, value: valor, date: DateTime.now());
+  addTransaction(String titlo, double valor) {
+    final newTransaction = Transaction(id: Random().nextDouble().toString(), title: titlo, value: valor, date: DateTime.now());
 
-   setState(() {
-     _litTransaction.add(newTransaction);
-   });
+    setState(() {
+      _litTransaction.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+    
+  }
+
+  transactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(
+            onSubmit: addTransaction,
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title:const Text('Despesas Pessoais'),
-      ),
-      body:SingleChildScrollView(
+      appBar: AppBar(title: const Text('Despesas Pessoais'), actions: [
+        IconButton(
+          onPressed: () {
+            transactionFormModal(context);
+          },
+          icon: const Icon(Icons.add),
+        ),
+      ]),
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment:CrossAxisAlignment.stretch,
-          children:[
-            const Card(
-              elevation:5,
-              color:Colors.blue,
-              child: Text('Gráfico')
-            ),
-          TransactionForm(onSubmit: addTransaction,),
-          TransactionList(lista: _litTransaction)
-          
-          ],
-          
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [const Card(elevation: 5, color: Colors.blue, child: Text('Gráfico')), TransactionList(lista: _litTransaction)],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          transactionFormModal(context);
+        },
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
