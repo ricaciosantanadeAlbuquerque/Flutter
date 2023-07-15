af6c0fde80829923d25ad2e6be287e9dee471da6
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-15 01:12:53 -0300

    início de projeto

---
+++ Flutter_udemy/exe_despesas_pessoais_16/android/app/src/main/kotlin/com/example/exe_despesas_pessoais_16/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exe_despesas_pessoais_16
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter_udemy/exe_despesas_pessoais_16/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpensesApp());
+}
+
+//==========================================Stateless================================================================================
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MyHomeApp();
+  }
+}
+
+//===========================================Stateful================================================================================
+class MyHomeApp extends StatefulWidget {
+  const MyHomeApp({super.key});
+  @override
+  State<MyHomeApp> createState() => MyHomeAppState();
+}
+
+//=========================================State<>===================================================================================
+class MyHomeAppState extends State<MyHomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title:const Text('Despesas Pessoais'),
+        actions:[
+          IconButton(onPressed:(){},icon:const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body:Column(),
+      floatingActionButton:FloatingActionButton(onPressed:(){},child: const Icon(Icons.add),
+      ),
+     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
