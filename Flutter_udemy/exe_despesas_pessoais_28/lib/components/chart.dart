4f77452a8f988e2c0d9f78f2efdb56247bd53e5c
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-10-02 18:27:40 -0300

    classe ChartBart

--- Flutter_udemy/exe_despesas_pessoais_28/lib/components/chart.dart
+++ Flutter_udemy/exe_despesas_pessoais_28/lib/components/chart.dart
@@ -1,45 +1,54 @@
+import 'package:exe_despesas_pessoais_28/components/chart_bart.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
-   final List<Transaction> listaTransaction;
+  final List<Transaction> listaTransaction;
 
-   const Chart({super.key,required this.listaTransaction});
+  const Chart({super.key, required this.listaTransaction});
 
-   List<Map<String,Object>> get groupedTransaction{
-
-    return List.generate(7,(index){
-      final weekDay = DateTime.now().subtract(Duration(days:index));
+  List<Map<String, Object>> get groupedTransaction {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
 
       double totlaSum = 0.0;
 
-      for(var i =0; i < listaTransaction.length;i++){
-
+      for (var i = 0; i < listaTransaction.length; i++) {
         bool sameDay = listaTransaction[i].date.day == weekDay.day;
         bool sameMonth = listaTransaction[i].date.month == weekDay.month;
         bool sameYear = listaTransaction[i].date.year == weekDay.year;
 
-        if(sameDay && sameMonth && sameYear){
-
-           totlaSum += listaTransaction[index].value;
-
+        if (sameDay && sameMonth && sameYear) {
+          totlaSum += listaTransaction[index].value;
         }
       }
-      return {'day':DateFormat.E().format(weekDay)[0],'value':totlaSum};
+      return {'day': DateFormat.E().format(weekDay)[0], 'value': totlaSum};
     });
-   }
+  }
+
+  get weekTotleSum {
+    return groupedTransaction.fold(0.0, (ct, map) {
+      return ct += (map['value'] as double);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation:6,
-      margin: const  EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransaction.map((map){
-          return Text('${map['day']} - ${map['value']}');
-        }).toList()
-      )
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          children: groupedTransaction.map((map) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(label: (map['day'] as String), value: (map['value'] as double), percentage: (map['value'] as double) / weekTotleSum),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
