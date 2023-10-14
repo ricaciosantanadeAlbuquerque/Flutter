a85d179d4a50239a8c3b9efea2a42fbdc602d40d
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-10-13 22:11:00 -0300

    classe Chart

---
+++ Flutter_udemy/exe_despesas_pessoais_29/lib/components/chart.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> listaTransaction;
+
+  const Chart({super.key, required this.listaTransaction});
+
+  List<Map<String, Object>> get groupedTransaction {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < listaTransaction.length; i++) {
+        bool sameDay = listaTransaction[i].date.day == weekDay.day;
+        bool sameMonth = listaTransaction[i].date.month == weekDay.month;
+        bool sameYear = listaTransaction[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += listaTransaction[i].value;
+        }
+      }
+
+      return {'day': 'T', 'value': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Row(
+          children: groupedTransaction.map((map) {
+        return Text('${map['day']} == ${map['value']}');
+      }).toList()),
+    );
+  }
+}
