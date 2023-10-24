55bb88a358033cc172ea8c10e4ae645c1a0dda58
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-10-24 00:08:45 -0300

    correção de erro

---
+++ Flutter_udemy/exe_despesas_pessoais_30/lib/components/transaction_list.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> listTransaction;
+
+  const TransactionList({super.key, required this.listTransaction});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 380,
+      child: ListView.builder(
+        itemCount: listTransaction.length,
+        itemBuilder: (_, index) {
+          final trs = listTransaction[index];
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.purple,
+                    ),
+                  ),
+                  child: Text(
+                    'R\$ ${trs.value.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(
+                    trs.title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    DateFormat('d MMM y').format(trs.date),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- Flutter_udemy/exe_despesas_pessoais_30/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_30/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+import 'package:exe_despesas_pessoais_30/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'components/transaction_list.dart';
 
 void main() => runApp(const ExpensesApp());
 
@@ -7,7 +10,10 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: ThemeData().copyWith());
+    return MaterialApp(
+      theme: ThemeData().copyWith(),
+      home: const MyHomeApp(),
+      );
   }
 }
 
@@ -19,10 +25,34 @@ class MyHomeApp extends StatefulWidget {
 }
 
 class MyHomeAppState extends State<MyHomeApp> {
+
+  final List<Transaction> _listaTransaction = [
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 211.3, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Novo Tênis de corrida', value: 310.33, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-    );
+
+          appBar:AppBar(
+            title: const Text('Despesas Pessoais'),
+          ),
+          body:  Column(
+            crossAxisAlignment:CrossAxisAlignment.stretch,
+            children: [
+              Card(
+                elevation:5,
+                color:Colors.blue,
+                child:Text('Gráfico'),
+              ),
+              TransactionList(listTransaction:_listaTransaction  ,),
+              Card(
+                elevation:5,
+                color: Colors.blue,
+                child: Text('Formulário')
+              )
+            ]
+          )
+    );  
   }
 }
