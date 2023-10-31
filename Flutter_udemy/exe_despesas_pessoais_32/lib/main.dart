ee49a852cf01d378d3acf77bbdfc2140b242157c
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-10-30 22:01:29 -0300

    listaTransaction

--- Flutter_udemy/exe_despesas_pessoais_32/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_32/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:exe_despesas_pessoais_32/model/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const ExpensesApp());
@@ -22,33 +25,32 @@ class MyHomeApp extends StatefulWidget {
 }
 
 class MyHomeAppSteta extends State<MyHomeApp> {
+
+  final List<Transaction> listaTransaction = [
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        appBar: AppBar(),
-        body:  const Column(
-          mainAxisAlignment:MainAxisAlignment.spaceAround,
-          crossAxisAlignment:CrossAxisAlignment.stretch,
-          children:[
-            Card(
-              elevation:5,
-              color:Colors.blue,
-              child: Text('Gráfico')
-            ),
-
-            Card(
-            elevation:5,
-            color:Colors.blue,
+      appBar: AppBar(),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(elevation: 5, color: Colors.blue, child: Text('Gráfico')),
+          Card(
+            elevation: 5,
+            color: Colors.blue,
             child: Text('Lista'),
-            ),
-            Card(
-              elevation:5,
-              color:Colors.blue,
-              child: Text('Formulário'),
-            ),
-          ],
-        ),
+          ),
+          Card(
+            elevation: 5,
+            color: Colors.blue,
+            child: Text('Formulário'),
+          ),
+        ],
+      ),
     );
   }
 }
