add1107483a582db8181945af4790f29b5f762a6
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-05 00:50:19 -0300

    classe Transaction

--- Flutter_udemy/exe_despesas_pessoais_33/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_33/lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
+import 'package:exe_despesas_pessoais_33/model/transaction.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
@@ -8,10 +11,10 @@ class ExpensesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData().copyWith(
-        colorScheme:ThemeData().colorScheme.copyWith(
-          primary:Colors.purple,
-          secondary:Colors.amberAccent,
-        ),
+        colorScheme: ThemeData().colorScheme.copyWith(
+              primary: Colors.purple,
+              secondary: Colors.amberAccent,
+            ),
       ),
       home: const MyHomeApp(),
     );
@@ -25,28 +28,32 @@ class MyHomeApp extends StatefulWidget {
 }
 
 class _MyHomeAppState extends State<MyHomeApp> {
-
+  final List<Transaction> listTransaction = [
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 211.30, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         title: const Text('Despesas pessoais'),
       ),
       body: const Column(
-        children:[
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
           Card(
-            elevation:5,
-            color:Colors.blue,
+            elevation: 5,
+            color: Colors.blue,
             child: Text('Gráfico'),
           ),
           Card(
-            elevation:5,
-            color:Colors.blue,
-            child:Text('Lista'),
+            elevation: 5,
+            color: Colors.blue,
+            child: Text('Lista'),
           ),
           Card(
-            elevation:5,
-            color:Colors.blue,
+            elevation: 5,
+            color: Colors.blue,
             child: Text('Formulário'),
           ),
         ],
