3ecea7c8d3a28b30cad0992b45bc959460f185ed
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-05 22:36:41 -0300

    chartBart classe Grafica

--- Flutter_udemy/exe_despesas_pessoais_33/lib/components/chart.dart
+++ Flutter_udemy/exe_despesas_pessoais_33/lib/components/chart.dart
@@ -2,6 +2,8 @@ import 'package:exe_despesas_pessoais_33/model/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import 'chart_bar.dart';
+
 class Chart extends StatelessWidget {
   final List<Transaction> listaTransaction;
   const Chart({super.key, required this.listaTransaction});
@@ -30,10 +32,16 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
-      child: Row(
-          children: groupedTransaction.map((map) {
-        return Text('${map['day']}  ${map['value']}');
-      }).toList()),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment:MainAxisAlignment.spaceAround,
+            children: groupedTransaction.map((map) {
+          return Flexible(
+            fit: FlexFit.tight,
+            child: ChartBar(label: map['day'] as String, value: map['value'] as double, percentage: 0.5));
+        }).toList()),
+      ),
     );
   }
 }
