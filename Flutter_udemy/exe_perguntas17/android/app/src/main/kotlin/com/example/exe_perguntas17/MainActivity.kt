8325af9f320c7c72239cf5bdad9c278c8ef55713
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-06 18:42:57 -0300

    app inicio

---
+++ Flutter_udemy/exe_perguntas17/android/app/src/main/kotlin/com/example/exe_perguntas17/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exe_perguntas17
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter_udemy/exe_perguntas17/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const PerguntaApp());
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+  @override
+  State<PerguntaApp> createState() => _PerguntaAppState();
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  int _index = 0;
+  int _valor = 0;
+
+  final List<Map<String, Object>> listaPergunta = const [
+    {
+      'texto': 'Qual é a sua cor favorita ?',
+      'resposta': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu Animal favorito ?',
+      'resposta': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito ?',
+      'resposta': [
+        {'texto': 'Leo', 'pontuacao': 10},
+        {'texto': 'Maria', 'pontuacao': 5},
+        {'texto': 'João', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
+      ],
+    },
+  ];
+
+  void _responder() {
+    setState(() {
+      _index++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> lista = listaPergunta[_index]['resposta'] as List<Map<String, Object>>;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Perguntas')),
+        body: Column(
+          crossAxisAlignment:CrossAxisAlignment.stretch,
+          children: [
+          Text(listaPergunta[_index]['texto'] as String),
+          ...lista.map((map) {
+            return ElevatedButton(
+              onPressed: _responder,
+              child: Text(map['texto'] as String),
+            );
+          }).toList()
+        ]),
+      ),
+    );
+  }
+}
