9e38f7224f399c9d01b3d8e438c6466559d427e3
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-06 23:52:11 -0300

    model classe Transaction

--- Flutter_udemy/exe_despesas_pessoais_34/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_34/lib/main.dart
@@ -10,6 +10,7 @@ class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme:ThemeData().copyWith(
         colorScheme:ThemeData().colorScheme.copyWith(
           primary: Colors.purple,
