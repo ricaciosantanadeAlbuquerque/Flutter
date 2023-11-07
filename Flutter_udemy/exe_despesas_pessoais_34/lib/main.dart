4bfc49accfd22b8fd198f2fd29c07d62724732d0
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-06 23:41:51 -0300

    inicio ExpensesApp

---
+++ Flutter_udemy/exe_despesas_pessoais_34/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:ThemeData().copyWith(
+        colorScheme:ThemeData().colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary:Colors.amberAccent,
+        ),
+      ),
+      home: const MyHomeApp(),
+    );
+  }
+}
+
+class MyHomeApp extends StatefulWidget {
+  const MyHomeApp({super.key});
+  @override
+  State<MyHomeApp> createState() => MyHomeAppState();
+}
+
+class MyHomeAppState extends State<MyHomeApp> {
+
+ @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+        title: const Center(
+          child: Text('Despesas Pessoais'),
+        ),
+        actions:[
+          IconButton(
+            onPressed:(){},
+            icon:const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body:  const Column(
+        mainAxisAlignment:MainAxisAlignment.spaceAround,
+        crossAxisAlignment:CrossAxisAlignment.stretch,
+        children:[
+          Card(
+            elevation:5,
+            color:Colors.blue,
+            child: Text('Gráfico'),
+          ),
+          Card(
+            elevation: 5,
+            color:Colors.blue,
+            child: Text('Lista Widget')
+          ),
+          Card(
+            elevation:5,
+            color:Colors.blue,
+            child:Text('Form'),
+          ),
+        ],
+      ),
+      floatingActionButton:FloatingActionButton(
+        onPressed:(){},
+        child:const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation:FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
