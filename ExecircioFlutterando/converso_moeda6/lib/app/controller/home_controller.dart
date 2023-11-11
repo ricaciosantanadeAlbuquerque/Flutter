9413e66dd8b53a3f4b77eb69f4400891d6e09523
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-10 23:51:16 -0300

    controller

---
+++ ExecircioFlutterando/converso_moeda6/lib/app/controller/home_controller.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../models/currency_model.dart';
+
+class HomeController {
+  final TextEditingController entrada;
+  final TextEditingController saida;
+  List<CurrencyModel>? listCurrencyModel;
+  CurrencyModel? toCurrency;
+  CurrencyModel? fromCurrency;
+
+  HomeController({required this.entrada, required this.saida}) {
+    listCurrencyModel = CurrencyModel.listCurrencyModel;
+    toCurrency = listCurrencyModel![0];
+    fromCurrency = listCurrencyModel![1];
+  }
+
+  void convert() {
+    String text = entrada.text;
+    double value = double.tryParse(text) ?? 1.0;
+    double retorno = 0;
+
+    if (fromCurrency!.name == 'Real') {
+      retorno = value * toCurrency!.real;
+    } else if (fromCurrency!.name == 'Dolar') {
+      retorno = value * toCurrency!.dolar;
+    } else if (fromCurrency!.name == 'Euro') {
+      retorno = value * toCurrency!.euro;
+    }
+
+    saida.text = retorno.toStringAsFixed(2);
+  }
+}
