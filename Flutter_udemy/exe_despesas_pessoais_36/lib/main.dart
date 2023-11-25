abb768c3119f4401adda57599205d50eecebe106
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-25 18:46:31 -0300

    lista construida

--- Flutter_udemy/exe_despesas_pessoais_36/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_36/lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'package:intl/intl.dart';
 import 'package:exe_despesas_pessoais_36/models/transaction.dart';
 import 'package:flutter/material.dart';
 
@@ -26,12 +26,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
-  
+
   final List<Transaction> listTransaction = [
-    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now())
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 255520.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+  Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 250.33, date: DateTime.now()),
   ];
-
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,21 +66,50 @@ class MyHomePageState extends State<MyHomePage> {
           child: Text('Despesas Pessoais'),
         ),
       ),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: Column(
+       // mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Card(
+          const Card(
             elevation: 5,
             color: Colors.blue,
             child: Text('Gráfico'),
           ),
-          Card(
-            elevation: 5,
-            color: Colors.blue,
-            child: Text('Lista'),
-          ),
-          Card(
+          SizedBox(
+              height: 380,
+              child: ListView.builder(
+                  itemCount: listTransaction.length,
+                  itemBuilder: (_, index) {
+                    final trs = listTransaction[index];
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                        vertical:8,
+                        horizontal:5,
+                      ),
+                      elevation: 5,
+                      child: ListTile(
+                        leading:CircleAvatar(
+                          radius:30,
+                          child: Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: FittedBox(
+                              child: Text(trs.value.toStringAsFixed(2),
+                              style:const TextStyle(
+                                fontWeight:FontWeight.bold,
+                              ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        title: Text(trs.title),
+                        subtitle:Text(DateFormat('dd MMM y').format(trs.date)),
+                        trailing: IconButton(
+                          color:Theme.of(context).colorScheme.error,
+                          onPressed:(){},icon:const Icon(Icons.delete)),
+                      ),
+                    );
+                  })),
+          const Card(
             elevation: 5,
             color: Colors.blue,
             child: Text('Form'),
