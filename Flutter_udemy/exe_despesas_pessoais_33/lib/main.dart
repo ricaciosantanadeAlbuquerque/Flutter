df3719e04a98f80aba4403702755faffd4f679bc
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-12-20 22:30:03 -0300

    responsividade na prática

--- Flutter_udemy/exe_despesas_pessoais_33/lib/main.dart
+++ Flutter_udemy/exe_despesas_pessoais_33/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'package:exe_despesas_pessoais_33/model/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'components/chart.dart';
 import 'components/transaction_form.dart';
 import 'components/transaction_list.dart';
@@ -11,6 +12,9 @@ class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
     return MaterialApp(
       theme: ThemeData().copyWith(
         colorScheme: ThemeData().colorScheme.copyWith(
@@ -25,9 +29,9 @@ class ExpensesApp extends StatelessWidget {
                 fontFamily: 'EBGaramond',
               ),
             ),
-        appBarTheme: const AppBarTheme(
+        appBarTheme: AppBarTheme(
           titleTextStyle: TextStyle(
-            fontSize: 25,
+            fontSize: 25 * MediaQuery.of(context).textScaleFactor,
             fontWeight: FontWeight.bold,
             fontFamily: 'Quicksand',
           ),
@@ -77,35 +81,45 @@ class _MyHomeAppState extends State<MyHomeApp> {
 
   List<Transaction> get _recentTransaction {
     return _listTransaction.where((trs) {
-      return trs.date.isAfter(DateTime.now().subtract(const Duration(days: 7),),);
+      return trs.date.isAfter(
+        DateTime.now().subtract(
+          const Duration(days: 7),
+        ),
+      );
     }).toList();
   }
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: const Text('Despesas pessoais'),
+      actions: [
+        IconButton(
+          onPressed: () {
+            _openTransactionFormModal(context);
+          },
+          icon: const Icon(Icons.add),
+        ),
+      ],
+    );
+    final altura = MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Despesas pessoais'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              _openTransactionFormModal(context);
-            },
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-
-            Chart(listaTransaction: _recentTransaction),
-
-            TransactionList(
-              listaTransaction: _listTransaction,
-              onRemove: _removeTransaction,
+            SizedBox(
+              height: altura * 0.25,
+              child: Chart(listaTransaction: _recentTransaction),
+            ),
+            SizedBox(
+              height: altura * 0.75,
+              child: TransactionList(
+                listaTransaction: _listTransaction,
+                onRemove: _removeTransaction,
+              ),
             ),
           ],
         ),
