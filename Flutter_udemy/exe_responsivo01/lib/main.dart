c6f5c67ec8f06816e65544e02b3e485db0196263
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-01-18 01:51:03 -0300

    Fixando a tela no modo retrato com

--- Flutter_udemy/exe_responsivo01/lib/main.dart
+++ Flutter_udemy/exe_responsivo01/lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:exe_responsivo01/view/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const ExpensesApp());
 
@@ -9,6 +9,13 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+   
+   /**
+    *  SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+    */
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData().copyWith(
@@ -20,8 +27,12 @@ class ExpensesApp extends StatelessWidget {
                 titleLarge: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black, fontFamily: 'Nunito'),
                 labelLarge: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16, fontFamily: 'Nunito'),
               ),
-          appBarTheme: const AppBarTheme(
-            titleTextStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, fontFamily: 'AlexBrush'),
+          appBarTheme:  AppBarTheme(
+            titleTextStyle: TextStyle(
+              fontSize: 25 * MediaQuery.of(context).textScaleFactor,
+            fontWeight: FontWeight.bold,
+             fontFamily: 'AlexBrush',
+             ),
           ),
         ),
         home: const MyHomePage());
