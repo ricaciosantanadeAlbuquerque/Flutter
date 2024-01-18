368c5c1573148ab6e2cf4c5d516ed37dde92c25d
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-01-18 01:41:47 -0300

    capturando o tamanho da leta do dispositivo

---
+++ Flutter_udemy/exe_responsivo01/lib/components/transaction_form.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionForm extends StatefulWidget {
+  final void Function(String, double, DateTime) onSubmitted;
+
+  const TransactionForm({super.key, required this.onSubmitted});
+
+  @override
+  State<TransactionForm> createState() => TransactionFormState();
+}
+
+class TransactionFormState extends State<TransactionForm> {
+  final title = TextEditingController();
+  final value = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
+  void submited() {
+    final titulo = title.text;
+    final valor = double.tryParse(value.text) ?? 0.0;
+
+    if (titulo.isEmpty || valor <= 0) {
+      return;
+    }
+
+    widget.onSubmitted(titulo, valor, selectedDate);
+  }
+
+  void calendar() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2023),
+      lastDate: DateTime.now(),
+    ).then((date) {
+      if (date == null) {
+        return;
+      }
+      setState(() {
+        selectedDate = date;
+      });
+
+      // print("Selecionado");
+    });
+
+    // print('Feito');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              controller: title,
+              onSubmitted: (_) => submited(),
+              decoration: const InputDecoration(
+                labelText: 'Título',
+              ),
+            ),
+            TextField(
+              controller: value,
+              onSubmitted: (_) => submited(),
+              keyboardType: const TextInputType.numberWithOptions(decimal: true),
+              decoration: const InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+            ),
+            SizedBox(
+              height: 70,
+              child: Row(
+                children: [
+                  Expanded(child: Text('Data Selecionada ${DateFormat('dd / MM / y').format(selectedDate)}')),
+                  TextButton(
+                    onPressed: calendar,
+                    child: const Text('Data Selecionada'),
+                  ),
+                ],
+              ),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              ElevatedButton(
+                onPressed: submited,
+                child: const Text('Nova transação'),
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
