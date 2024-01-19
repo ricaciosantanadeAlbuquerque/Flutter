d0a4567c9697ba5e691bae8f093b1c3af2452bbd
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-01-19 15:40:55 -0300

    responsividade

---
+++ Flutter_udemy/exe_responsivo04/lib/components/transaction_list.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../model/transaction.dart';
+
+class TransactionLits extends StatelessWidget {
+  final List<Transaction> listTransaction;
+  final void Function(String) onSubmitted;
+
+  const TransactionLits({super.key, required this.listTransaction, required this.onSubmitted});
+
+  @override
+  Widget build(BuildContext context) {
+    return listTransaction.isEmpty
+        ? LayoutBuilder(builder: (context, constraints) {
+            return Column(
+              children: [
+                     SizedBox(height: constraints.maxHeight * 0.05),
+                Text('Nenhuma Transação Cadastrada !', style: Theme.of(context).textTheme.titleLarge),
+                     SizedBox(
+                  height: constraints.maxHeight * 0.05,
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.60,
+                  child: Image.asset('assets/image/waiting.png', fit: BoxFit.cover),
+                ),
+              ],
+            );
+          })
+        : ListView.builder(
+            itemCount: listTransaction.length,
+            itemBuilder: (_, index) {
+              final trs = listTransaction[index];
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                elevation: 5,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(9.0),
+                      child: FittedBox(
+                        child: Text(
+                          trs.value.toStringAsFixed(2),
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                      ),
+                    ),
+                  ),
+                  title: Text(trs.title, style: Theme.of(context).textTheme.titleLarge),
+                  subtitle: Text(DateFormat('dd MMM y').format(trs.date)),
+                  trailing: IconButton(
+                    onPressed: () {
+                      onSubmitted(listTransaction[index].id);
+                    },
+                    color: Theme.of(context).colorScheme.error,
+                    icon: const Icon(Icons.delete),
+                  ),
+                ),
+              );
+            },
+          );
+  }
+}
