f662d64e754d7970090629d32335bea61580f9ff
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-01-25 02:17:23 -0300

    add TextButton.icon

--- Flutter_udemy/exe_responsivo06/lib/components/transaction_list.dart
+++ Flutter_udemy/exe_responsivo06/lib/components/transaction_list.dart
@@ -15,7 +15,7 @@ class TransactionLits extends StatelessWidget {
         ? LayoutBuilder(builder: (context, constraints) {
             return Column(
               children: [
-                 SizedBox(height: constraints.maxHeight * 0.05),
+                SizedBox(height: constraints.maxHeight * 0.05),
                 Text('Nenhuma Transação Cadastrada !', style: Theme.of(context).textTheme.titleLarge),
                 SizedBox(
                   height: constraints.maxHeight * 0.05,
@@ -49,14 +49,31 @@ class TransactionLits extends StatelessWidget {
                     ),
                   ),
                   title: Text(trs.title, style: Theme.of(context).textTheme.titleLarge),
-                  subtitle: Text(DateFormat('dd MMM y').format(trs.date)),
-                  trailing: IconButton(
-                    onPressed: () {
-                      onSubmitted(listTransaction[index].id);
-                    },
-                    color: Theme.of(context).colorScheme.error,
-                    icon: const Icon(Icons.delete),
+                  subtitle: Text(
+                    DateFormat('dd MMM y').format(trs.date),
                   ),
+                  trailing: MediaQuery.of(context).size.width > 480
+                      ? TextButton.icon(
+                          onPressed: () {
+                            onSubmitted(trs.id);
+                          },
+                          icon: Icon(
+                            Icons.delete,
+                            color: Theme.of(context).colorScheme.error,
+                          ),
+                          label:Text('Excluir',
+                          style:TextStyle(
+                          color:Theme.of(context).colorScheme.error,
+                          ),
+                          ),
+                        )
+                      : IconButton(
+                          onPressed: () {
+                            onSubmitted(listTransaction[index].id);
+                          },
+                          color: Theme.of(context).colorScheme.error,
+                          icon: const Icon(Icons.delete),
+                        ),
                 ),
               );
             },
