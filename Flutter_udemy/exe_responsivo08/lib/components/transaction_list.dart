dad9ea02aae1699a780446eb8a29b01802118e4f
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-02-06 22:16:00 -0300

    TextButton.icon Rederização condiconal via ternário

--- Flutter_udemy/exe_responsivo08/lib/components/transaction_list.dart
+++ Flutter_udemy/exe_responsivo08/lib/components/transaction_list.dart
@@ -51,13 +51,27 @@ class TransactionLits extends StatelessWidget {
                     ),
                     title: Text(trs.title, style: Theme.of(context).textTheme.titleLarge),
                     subtitle: Text(DateFormat('dd MMM y').format(trs.date)),
-                    trailing: IconButton(
-                      onPressed: () {
-                        onSubmitted(listTransaction[index].id);
-                      },
-                      color: Theme.of(context).colorScheme.error,
-                      icon: const Icon(Icons.delete),
-                    ),
+                    trailing: MediaQuery.of(context).size.width > 480
+                        ? TextButton.icon(
+                            onPressed: () {
+                              onSubmitted(trs.id);
+                            },
+                            icon: Icon(Icons.delete,
+                            color:Theme.of(context).colorScheme.error,
+                            ),
+                            label: Text('Excluindo',
+                            style:TextStyle(
+                              color:Theme.of(context).colorScheme.error,
+                            ),
+                            ),
+                          )
+                        : IconButton(
+                            onPressed: () {
+                              onSubmitted(listTransaction[index].id);
+                            },
+                            color: Theme.of(context).colorScheme.error,
+                            icon: const Icon(Icons.delete),
+                          ),
                   ),
                 );
               },
