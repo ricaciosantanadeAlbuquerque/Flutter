3e4279845d33bbb2fd5b720981876088dd084d4c
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-02-16 03:33:46 -0300

    responsividade

---
+++ Flutter_udemy/exe_responsivo12/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:exe_responsivo12/view/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData().copyWith(
+          colorScheme: ThemeData().colorScheme.copyWith(
+                primary: Colors.purple,
+                secondary: Colors.amberAccent,
+              ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black, fontFamily: 'Nunito'),
+                labelLarge: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16, fontFamily: 'Nunito'),
+              ),
+          appBarTheme: const AppBarTheme(
+            titleTextStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, fontFamily: 'AlexBrush'),
+          ),
+        ),
+        home: const MyHomePage());
+  }
+}
