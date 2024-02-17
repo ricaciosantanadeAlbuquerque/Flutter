eb98c8fd6550e59729784ca0f31b32d8af9ebeb3
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-02-17 17:45:50 -0300

    Adaptatividade com Platform.isIOS

--- Flutter_udemy/exe_responsivo13/lib/view/home_page.dart
+++ Flutter_udemy/exe_responsivo13/lib/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../components/chart.dart';
 import '../components/transaction_form.dart';
@@ -52,93 +53,101 @@ class MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Widget getIconButton(Function() fn,IconData icon) {
+    return Platform.isIOS ? GestureDetector(onTap: fn, child: Icon(icon)) : IconButton(onPressed: fn, icon: Icon(icon));
+  }
+
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    final paisagem =mediaQuery.orientation == Orientation.landscape;
+    final paisagem = mediaQuery.orientation == Orientation.landscape;
+    final arrayActions = [
+      getIconButton(
+        () {
+          setState(() {
+            showChart = !showChart;
+          });
+        },
+        showChart ? Icons.list : Icons.show_chart,
+      ),
+      getIconButton(
+         () {
+          opeTransactionFormModal(context);
+        },
+         Platform.isIOS ? CupertinoIcons.add : Icons.add,
+      ),
+    ];
 
     final appBar = AppBar(
-      title: const Text(
-        'Despesas Pessoais',
-      ),
-      actions: [
-        IconButton(
-          onPressed: () {
-            opeTransactionFormModal(context);
-          },
-          icon: const Icon(Icons.add),
-        ),
-      ],
-    );
-
-    final alturaApp =mediaQuery.size.height - appBar.preferredSize.height -mediaQuery.padding.top;
-
-    return Scaffold(
-      appBar: AppBar(
         title: const Text(
           'Despesas Pessoais',
         ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              setState(() {
-                showChart = !showChart;
-              });
-            },
-            icon: Icon(showChart ? Icons.list : Icons.show_chart),
-          ),
-          IconButton(
-            onPressed: () {
-              opeTransactionFormModal(context);
-            },
-            icon: const Icon(Icons.add),
-          ),
+        actions: arrayActions);
+
+    final alturaApp = mediaQuery.size.height - appBar.preferredSize.height - mediaQuery.padding.top;
+
+    final bodyPage = SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          if (paisagem)
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(showChart ? 'Mostrando o Gráfico' : 'Mostrando a Lista'),
+              Switch.adaptive(
+                  activeColor: Theme.of(context).colorScheme.secondary,
+                  value: showChart,
+                  onChanged: (value) {
+                    setState(() {
+                      showChart = value;
+                    });
+                  }),
+            ]),
+          if (showChart || !paisagem)
+            SizedBox(
+              height: alturaApp * (paisagem ? 0.80 : 0.25),
+              child: Chart(
+                listaTransaction: recentTransaction,
+              ),
+            ),
+          if (!showChart || !paisagem)
+            SizedBox(
+              height: alturaApp * (paisagem ? 1 : 0.75),
+              child: TransactionLits(
+                listTransaction: listTransaction,
+                onSubmitted: removeTransactio,
+              ),
+            ), // comunicação dirate / comunicação indireta
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            if (paisagem)
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Text(showChart ? 'Mostrando o Gráfico' : 'Mostrando a Lista'),
-                Switch.adaptive(
-                   activeColor: Theme.of(context).colorScheme.secondary,
-                    value: showChart,
-                    onChanged: (value) {
-                      setState(() {
-                        showChart = value;
-                      });
-                    }),
-              ]),
-            if (showChart || !paisagem)
-              SizedBox(
-                height: alturaApp * (paisagem ? 0.80 : 0.25),
-                child: Chart(
-                  listaTransaction: recentTransaction,
-                ),
-              ),
-            if (!showChart || !paisagem)
-              SizedBox(
-                height: alturaApp * (paisagem ? 1 : 0.75),
-                child: TransactionLits(
-                  listTransaction: listTransaction,
-                  onSubmitted: removeTransactio,
-                ),
-              ), // comunicação dirate / comunicação indireta
-          ],
-        ),
-      ),
-      floatingActionButton: Platform.isIOS ?
-       Container()
-       :
-       FloatingActionButton(
-        onPressed: () {
-          opeTransactionFormModal(context);
-        },
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+              middle: const Text('Despesas Pessoais'),
+               trailing: Row(
+                mainAxisSize:MainAxisSize.min,
+                children: arrayActions,
+                ),
+               ),
+            child: bodyPage,
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'Despesas Pessoais',
+              ),
+            ),
+            body: bodyPage,
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : FloatingActionButton(
+                    onPressed: () {
+                      opeTransactionFormModal(context);
+                    },
+                    child: const Icon(Icons.add),
+                  ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          );
   }
 }
