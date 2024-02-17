fec33df841db8e82a908d4213c84e51dd98cf503
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-02-17 18:18:25 -0300

    AdaptativeButton

---
+++ Flutter_udemy/exe_responsivo13/lib/components/adaptative_button.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdaptativeButton extends StatelessWidget {
+  final String label;
+  final void Function() onPressed;
+
+  const AdaptativeButton({super.key, required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            onPressed: onPressed,
+            color: Theme.of(context).colorScheme.primary,
+            child: Text(label),
+          )
+        : ElevatedButton(
+           style: ElevatedButton.styleFrom(
+            backgroundColor:Theme.of(context).colorScheme.primary
+           ),
+            onPressed: onPressed,
+            child: Text(label),
+          );
+  }
+}
